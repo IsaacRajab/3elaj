60167d6bf3a6c98670431ef32d0bb8394840ab22
IsaacRajab <isaacrajab17@gmail.com>
2023-02-12 01:44:10 +0200

    Contact us with feedback emails

--- app/src/main/java/com/example/a3elaj/ContactUS.java
+++ app/src/main/java/com/example/a3elaj/ContactUS.java
@@ -1,8 +1,13 @@
 package com.example.a3elaj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactUS extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class ContactUS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        Spinner spinner= findViewById(R.id.feedback);
+        TextView textView =findViewById(R.id.msg);
+        String text = spinner.getSelectedItem().toString();
+        Button btn=findViewById(R.id.send);
+        btn.setOnClickListener(v -> {
+            Intent i =new Intent(Intent.ACTION_SEND);
+            i.setType("message/html");
+            i.putExtra(Intent.EXTRA_EMAIL, "test@test.com");
+            i.putExtra(Intent.EXTRA_SUBJECT,"Feedback From app");
+            i.putExtra(Intent.EXTRA_TEXT,"Feedback type:"+text+"\n Message :"+textView.getText());
+            try {
+                startActivity(Intent.createChooser(i,"Please Select Email"));
+            }catch (android.content.ActivityNotFoundException ex){
+                Toast.makeText(ContactUS.this,"There are no Email Clients",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
